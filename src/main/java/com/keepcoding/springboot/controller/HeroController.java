fa1b837b61ae37946871acf165da4b7b9068b91e
Manu <manu.villar.vitoria@gmail.com>
2021-11-02 16:53:19 +0100

    Adding a location with the new URI in the headers and changing the status 200 to status 201(created) in the  POST method

--- src/main/java/com/keepcoding/springboot/controller/HeroController.java
+++ src/main/java/com/keepcoding/springboot/controller/HeroController.java
@@ -4,9 +4,12 @@ import com.keepcoding.springboot.dao.HeroDaoService;
 import com.keepcoding.springboot.model.Hero;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.annotation.CreatedBy;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.PipedOutputStream;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -29,8 +32,13 @@ public class HeroController {
     //Devolver estado 201:Created
     //Devolver URI del nuevo recurso creado
     @PostMapping("/hero")
-    public Hero addHero(@RequestBody Hero hero) {
-        return heroDaoService.addHero(hero);
+    public ResponseEntity<Object> addHero(@RequestBody Hero hero) {
+        Hero addedHero =  heroDaoService.addHero(hero);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(addedHero.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
 }
